acbaef91c124c7abde4f728bc3dd60e8dc670ea1
Camila Queiroz <camilaqueiroz@mbp-de-camila.home>
2021-08-12 14:03:28 -0300

    error handling

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+import { EnsureAuthenticated } from "../middlewares/EnsureAuthenticated";
 import { CreateSpecificationController } from "../modules/cars/useCases/createSpecification/CreateSpecificationController";
 import { ListSpecificationController } from "../modules/cars/useCases/listSpecification/ListSpecificationController";
 
@@ -7,6 +8,7 @@ const SpecificationsRoutes = Router();
 const createSpecificationController = new CreateSpecificationController();
 const listSpecificationController = new ListSpecificationController();
 
+SpecificationsRoutes.use(EnsureAuthenticated);
 SpecificationsRoutes.post("/", createSpecificationController.handle);
 
 SpecificationsRoutes.get("/", listSpecificationController.handle);
